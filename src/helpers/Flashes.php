28ff3c99dd2ddce5bfa3d206bc126d6188f52372
Thomas Bouillon <tbouillon54@gmail.com>
2021-01-08 16:16:00 +0100

    feat: helper pour les flashs

---
+++ src/helpers/Flashes.php
@@ -0,0 +1,35 @@
+<?php
+namespace Whishlist\helpers;
+
+if(session_status() == PHP_SESSION_NONE)
+    session_start();
+
+/**
+ * Classe pour ajouter des flashs
+ */
+class Flashes 
+{   
+    /**
+     * Ajoute une flash en session
+     *
+     * @param string $message
+     * @param string $type
+     * @return void
+     */
+    public static function addFlash(string $message, string $type): void
+    {
+        $_SESSION['flashes'][] = [$type => $message];
+    }
+
+    /**
+     * Getter de touts les flash en session
+     *
+     * @return array flashes
+     */
+    public static function getFlashes(): array
+    {
+        $arr = $_SESSION['flashes'] ?? [];
+        unset($_SESSION['flashes']);
+        return $arr;
+    } 
+}
